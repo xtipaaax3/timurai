b769dc1f603a4cb6a3e31679dde6f38ec3c52f4d
Timur <zupcevict@gmail.com>
2023-04-12 23:50:51 +0200

    update

--- client/script.js
+++ client/script.js
@@ -4,7 +4,7 @@ import user from './assets/user.svg'
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
 
-let loadInterval;
+let loadInterval
 
 function loader(element) {
     element.textContent = ''
@@ -19,43 +19,49 @@ function loader(element) {
         }
     }, 300);
 }
+
 function typeText(element, text) {
-  let index = 0
+    let index = 0
 
-  let interval = setInterval(() => {
-      if (index < text.length) {
-          element.innerHTML += text.charAt(index)
-          index++
-      } else {
-          clearInterval(interval)
-      }
-  }, 20)
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
 }
 
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
 function generateUniqueId() {
-  const timestamp = Date.now();
-  const randomNumber = Math.random();
-  const hexadecimalString = randomNumber.toString(16);
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
 
-  return `id-${timestamp}-${hexadecimalString}`;
+    return `id-${timestamp}-${hexadecimalString}`;
 }
+
 function chatStripe(isAi, value, uniqueId) {
-  return (
-      `
-      <div class="wrapper ${isAi && 'ai'}">
-          <div class="chat">
-              <div class="profile">
-                  <img 
-                    src=${isAi ? bot : user} 
-                    alt="${isAi ? 'bot' : 'user'}" 
-                  />
-              </div>
-              <div class="message" id=${uniqueId}>${value}</div>
-          </div>
-      </div>
-  `
-  )
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
 }
+
 const handleSubmit = async (e) => {
     e.preventDefault()
 
@@ -80,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://timurai.onrender.com', {
+    const response = await fetch('https://timurai.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -95,13 +101,13 @@ const handleSubmit = async (e) => {
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim()
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
 
         typeText(messageDiv, parsedData)
     } else {
         const err = await response.text()
 
-        messageDiv.innerHTML = "Nešto nije u redu, kontaktirajte Timura"
+        messageDiv.innerHTML = "Something went wrong"
         alert(err)
     }
 }
