ef19edd4a626777f5926b3c4bb9b3075b110a868
Timur <zupcevict@gmail.com>
2023-04-13 20:48:13 +0200

    apdejt

--- client/script.js
+++ client/script.js
@@ -14,7 +14,7 @@ function loader(element) {
         element.textContent += '.';
 
         // If the loading indicator has reached three dots, reset it
-        if (element.textContent === '....') {
+        if (element.textContent === 'Hmmm...') {
             element.textContent = '';
         }
     }, 300);
