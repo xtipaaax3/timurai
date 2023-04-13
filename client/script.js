86332b753532ee1c5cbd5ec6742166e27afe2b52
Timur <zupcevict@gmail.com>
2023-04-13 20:49:13 +0200

    apdejt

--- client/script.js
+++ client/script.js
@@ -11,7 +11,7 @@ function loader(element) {
 
     loadInterval = setInterval(() => {
         // Update the text content of the loading indicator
-        element.textContent += '.';
+        element.textContent += 'Hmmm...';
 
         // If the loading indicator has reached three dots, reset it
         if (element.textContent === 'Hmmm...') {
