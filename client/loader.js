e29dfad6eca857c52397ed069a044ebf2fbea586
Timur <zupcevict@gmail.com>
2023-05-06 00:53:38 +0200

    NEZ

--- client/loader.js
+++ client/loader.js
@@ -9,14 +9,3 @@ function updateLoader() {
   loadingNumber.innerHTML = load;
   loadingCircle.style.background = 'conic-gradient(from 0deg at 50% 50%, rgba(111, 123, 247, 1) 0%, rgba(155, 248, 244, 1) ' + load + '%, #101012 ' + load + '%)'
 }
-
-
-
-function funkcijabro()
-{
-  document.getElementById("ideovoideovo").style.display = "none"
-  document.getElementById("jaojjebotew").style.display = "block"
-  document.getElementById("neznamaaa").style.display = "block"
-  document.getElementById("stajlovi").disabled=false;
-  document.getElementById("stajleribrt").disabled=true
-}
