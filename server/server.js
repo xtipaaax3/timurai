96da20b0ab2dac75d8be87b4a8108d1caf5de8f4
Timur <zupcevict@gmail.com>
2023-05-12 23:47:13 +0200

    TIMUR AI v1.5

--- server/server.js
+++ server/server.js
@@ -26,6 +26,11 @@ app.post('/', async (req, res) => {
     const prompt = req.body.prompt;
     const response = await openai.createCompletion({
       model: "text-davinci-003",
+      messages: [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "Jebac."},
+        {"role": "user", "content": "Where was it played?"} ],
       prompt: `${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
@@ -34,9 +39,6 @@ app.post('/', async (req, res) => {
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
-    res.status(200).send({
-      bot: response.data.choices[0].text
-    });
 
   } catch (error) {
     console.error(error)
